00a674e6ae37fd0f3d4c6e8edea74c30a6127ef2
Brindobob <102769633+Brindobob@users.noreply.github.com>
2025-04-07 21:27:43 -0400

    add all fields for AddDialog component: name, photo, category, date

--- src/components/dialogs/AddDialog.jsx
+++ src/components/dialogs/AddDialog.jsx
@@ -9,20 +9,31 @@ import {
   FileUpload,
   Float,
   useFileUploadContext,
+  Select,
+  createListCollection,
 } from "@chakra-ui/react";
 import { LuFileImage, LuX } from "react-icons/lu";
+
+const plushieCategories = createListCollection({
+  items: [
+    { label: "Spring", value: "spring" },
+    { label: "Summer", value: "Summer" },
+    { label: "Fall", value: "Fall" },
+    { label: "Winter", value: "Winter" },
+  ],
+});
 
 const FileUploadList = () => {
   const fileUpload = useFileUploadContext();
   const files = fileUpload.acceptedFiles;
   if (files.length === 0) return null;
   return (
-    <FileUpload.ItemGroup pl="6">
+    <FileUpload.ItemGroup pt="4">
       {files.map((file) => (
         <FileUpload.Item
           w="auto"
           boxSize="20"
-          p="2"
+          p="1"
           file={file}
           key={file.name}
         >
@@ -52,31 +63,52 @@ function AddDialog() {
             <Dialog.Title>Add Plushie</Dialog.Title>
           </Dialog.Header>
           <Dialog.Body pb="4"></Dialog.Body>
-          <Stack gap="4">
-            <Field.Root pl="6" pr="6">
-              <Field.Label>Name</Field.Label>
-              <Input></Input>
-            </Field.Root>
+          <Stack pl="6" pr="6" gap="4">
             <Field.Root>
-              <Field.Label pl="6">Photo</Field.Label>
+              <Field.Label>Name</Field.Label>
+              <Input w="64"></Input>
             </Field.Root>
-            <FileUpload.Root accept="image/png">
+            <FileUpload.Root accept="image/png" gap="1.5">
               <FileUpload.HiddenInput />
+              <FileUpload.Label>Photo</FileUpload.Label>
               <FileUpload.Trigger asChild>
-                <Button variant="outline" size="sm" ml="6">
+                <Button w="64" variant="outline" size="sm">
                   <LuFileImage /> Upload Images
                 </Button>
               </FileUpload.Trigger>
               <FileUploadList />
             </FileUpload.Root>
+            <Select.Root collection={plushieCategories} size="sm" w="64">
+              <Select.HiddenSelect />
+              <Select.Label>Category</Select.Label>
+              <Select.Control>
+                <Select.Trigger>
+                  <Select.ValueText placeholder="Select category" />
+                </Select.Trigger>
+                <Select.IndicatorGroup>
+                  <Select.Indicator />
+                </Select.IndicatorGroup>
+              </Select.Control>
+              <Select.Positioner>
+                <Select.Content>
+                  {plushieCategories.items.map((plushieCategories) => (
+                    <Select.Item
+                      item={plushieCategories}
+                      key={plushieCategories.value}
+                    >
+                      {plushieCategories.label}
+                      <Select.ItemIndicator />
+                    </Select.Item>
+                  ))}
+                </Select.Content>
+              </Select.Positioner>
+            </Select.Root>
             <Field.Root>
-              <Field.Label pl="6">Category</Field.Label>
-            </Field.Root>
-            <Field.Root>
-              <Field.Label pl="6">Date</Field.Label>
+              <Field.Label>Date</Field.Label>
+              <Input w="64" />
             </Field.Root>
           </Stack>
-          <Dialog.Footer>
+          <Dialog.Footer mt="4">
             <Dialog.ActionTrigger asChild>
               <Button variant="outline">Cancel</Button>
             </Dialog.ActionTrigger>
